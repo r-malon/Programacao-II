aa387b4eb2d49265d2dec5aba0a6af69bec39617
luigilopes <7luiz.lopes7@gmail.com>
2019-04-25 10:54:15 -0300

    Fixed POST

--- routes.py
+++ routes.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect
+from flask import Flask, request, render_template, redirect, session
 from hashlib import sha256
 
 class User:
@@ -16,42 +16,44 @@ def user_exists(name):
 
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+app.config['SECRET_KEY'] = 'hey_baby'
 user_list = []
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
 	return render_template('index.html', user_list=user_list)
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/login', methods=['POST'])
 def login():
 	msg = None
-	if request.method == 'GET':
-		name = request.args.get('name')
-		password = request.args.get('password')
-		for user in user_list:
-			if user.check_psw(password) and user.name == name:
-				msg = "Logged in"
-			else:
-				msg = "Error!"
+	name = request.form['name']
+	password = request.form['password']
+	for user in user_list:
+		if user.check_psw(password) and user.name == name:
+			msg = "Logged in"
+		else:
+			msg = "Error!"
 	return render_template('login.html', msg=msg)
 
-@app.route('/signup', methods=['GET', 'POST'])
+@app.route('/form_login')
+def form_login():
+	return render_template('login.html')
+
+@app.route('/signup', methods=['POST'])
 def signup():
 	msg = None
-	if request.method == 'GET':
-		name = request.args.get('name')
-		password = request.args.get('password')
-		if user_exists(name):
-			msg = "User already exists!"
-		else:
-			user_list.append(User(name, password))
-			msg = "Sucess!"
+	session['name'] = request.form['name']
+	session['password'] = request.form['password']
+	if user_exists(session['name']):
+		msg = "User already exists!"
+	else:
+		user_list.append(User(session['name'], session['password']))
+		msg = "Sucess!"
 	return render_template('signup.html', msg=msg)
 
-@app.route('/delete', methods=['GET', 'POST'])
-def delete():
-	if request.method == 'POST':
-		pass
+@app.route('/form_signup', methods=['GET'])
+def form_signup():
+	return render_template('signup.html')
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', debug=True)
